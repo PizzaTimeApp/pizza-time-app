e6dae5355b38c04cbfddf6bb5178eb1a8562b842
Thomas DUBUIS <thomasdubuis13@gmail.com>
2022-01-05 19:22:04 +0100

    Feat : register page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,17 @@ import { IonicStorageModule } from '@ionic/storage';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './services/authInterceptor';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot({name: 'app',
   driverOrder: ['localstorage']})],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy},{
+    provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true
+    }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
